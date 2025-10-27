5caf2b78282d24fed84f4a1d80a8479954239f50
Varshini180906 <varshinioff.18@gmail.com>
2025-10-27 20:29:21 +0530

    Add files via upload

---
+++ assignment1.1.py
@@ -0,0 +1,26 @@
+#mathematical operations
+a=input("enter a: ")
+a=int(a)
+b=input("enter b: ")
+b=int(b)
+c=a+b
+c=int(c)
+print('addition: ', c)
+c=a-b
+c=int(c)
+print('subtraction: ', c)
+c=a*b
+c=int(c)
+print('multiplication: ', c)
+c=a/b
+c=int(c)
+print('division: ', c)
+
+
+
+
+
+
+
+
+
---
+++ assignment1.2.py
@@ -0,0 +1,5 @@
+#greeting
+firstname=str(input("enter first name: "))
+lastname=str(input("enter last name: "))
+name=firstname+" "+lastname +"!"
+print("hello,", (name),"welcome to the python program.")
